3038e2e4a04a6658567a54214db041a80047eced
MAGNANT Leo <leo.magnant@ynov.com>
2023-11-23 15:26:16 +0100

    modif bouton register et email username

--- src/components/utilities/Elements.jsx
+++ src/components/utilities/Elements.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const RoundedBoutton = ({handleClick, text, className}) => {
+export const RoundedBoutton = ({handleClick, text, className, disabled}) => {
     return (
-        <button className={`w-full py-3 text-black font-bold text-xl rounded-full ${className}`} onClick={handleClick}>
+        <button className={`w-full py-3 text-black font-bold text-xl rounded-full ${className}`}  onClick={handleClick} disabled={disabled}>
             {text}
         </button>
     );
